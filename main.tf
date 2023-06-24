1f6a34b39bc62e5af05affb2230eb294279ae188
vjsmit <vj.smit97@gmail.com>
2023-06-24 22:22:27 +0530

    make-destroy

--- main.tf
+++ main.tf
@@ -42,17 +42,16 @@ resource "aws_iam_role" "role" {
   })
 }
 
-#Attaching policy to role
-
-resource "aws_iam_role_policy_attachment" "policy-attach" {
-  role       = aws_iam_role.role.name
-  policy_arn = aws_iam_policy.policy.arn
-}
-
 #instance_profile
 resource "aws_iam_instance_profile" "instance_profile" {
   name = "${var.component}-${var.env}-instance-profile"
   role = aws_iam_role.role.name
+}
+
+#Attaching policy to role
+resource "aws_iam_role_policy_attachment" "policy-attach" {
+  role       = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.policy.arn
 }
 
 #security group
