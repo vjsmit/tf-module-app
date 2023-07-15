0fab920b883d7fdd6af64c6638db21999869cd0c
smvijay <vj.smit97@gmail.com>
2023-07-15 09:47:37 -0400

    configuring tags

--- main.tf
+++ main.tf
@@ -86,9 +86,11 @@ resource "aws_instance" "instance" {
   instance_type = "t3.small"
   vpc_security_group_ids = [aws_security_group.sg.id]
   iam_instance_profile = aws_iam_instance_profile.instance_profile.name
-  tags = {
+
+  tags = merge ({
     Name = "${var.component}-${var.env}"
-  }
+    },
+    var.tags)
 }
 
 #dns record
--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
 variable "env" {}
 
 variable "component" {}
+
+variable "tags" {
+  default = {}
+}
