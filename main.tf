e67e13fd09307881202bcd48dced58b659c29825
vjsmit <vj.smit97@gmail.com>
2023-08-05 22:48:59 +0530

    updating userdata.sh

--- main.tf
+++ main.tf
@@ -1,139 +1,15 @@
-##policy
-#resource "aws_iam_policy" "policy" {
-#  name        = "${var.component}-${var.env}-ssm-pm-policy"
-#  path        = "/"
-#  description = "${var.component}-${var.env}-ssm-pm-policy"
-#
-#  policy = jsonencode({
-#    "Version": "2012-10-17",
-#    "Statement": [
-#      {
-#        "Sid": "VisualEditor0",
-#        "Effect": "Allow",
-#        "Action": [
-#          "ssm:GetParameterHistory",
-#          "ssm:GetParametersByPath",
-#          "ssm:GetParameters",
-#          "ssm:GetParameter"
-#        ],
-#        "Resource": "arn:aws:ssm:us-east-1:434623354977:parameter/roboshop.${var.env}.${var.component}.*"
-#      }
-#    ]
-#  })
-#}
-#
-##IAM Role
-#
-#resource "aws_iam_role" "role" {
-#  name = "${var.component}-${var.env}-ec2-role"
-#
-#  assume_role_policy = jsonencode({
-#    Version = "2012-10-17"
-#    Statement = [
-#      {
-#        Action = "sts:AssumeRole"
-#        Effect = "Allow"
-#        Sid    = ""
-#        Principal = {
-#          Service = "ec2.amazonaws.com"
-#        }
-#      },
-#    ]
-#  })
-#}
-#
-##instance_profile_creation
-#resource "aws_iam_instance_profile" "instance_profile" {
-#  name = "${var.component}-${var.env}-instance-profile"
-#  role = aws_iam_role.role.name
-#}
-#
-##Attaching policy to role
-#resource "aws_iam_role_policy_attachment" "policy-attach" {
-#  role       = aws_iam_role.role.name
-#  policy_arn = aws_iam_policy.policy.arn
-#}
-#
-##security group
-#
-#resource "aws_security_group" "sg" {
-#  name        = "${var.component}-${var.env}-sg"
-#  description = "${var.component}-${var.env}-sg"
-#
-#  ingress {
-#    from_port        = 0
-#    to_port          = 0
-#    protocol         = "-1"
-#    cidr_blocks      = ["0.0.0.0/0"]
-#  }
-#
-#  egress {
-#    from_port        = 0
-#    to_port          = 0
-#    protocol         = "-1"
-#    cidr_blocks      = ["0.0.0.0/0"]
-#  }
-#
-#  tags = {
-#    Name = "${var.component}-${var.env}-sg"
-#  }
-#}
-#
-##ec2
-#
-#resource "aws_instance" "instance" {
-#  ami = data.aws_ami.ami.id
-#  instance_type = "t3.small"
-#  vpc_security_group_ids = [aws_security_group.sg.id]
-#  iam_instance_profile = aws_iam_instance_profile.instance_profile.name
-#
-#  tags = merge ({
-#    Name = "${var.component}-${var.env}"
-#    },
-#    var.tags)
-#}
-#
-##dns record
-#
-#resource "aws_route53_record" "dns" {
-#  zone_id = "Z0478111B3O54DLF4LSS"
-#  name    = "${var.component}-dev"
-#  type    = "A"
-#  ttl     = 30
-#  records = [aws_instance.instance.private_ip]
-#}
-#
-##null resource
-#
-#resource "null_resource" "ansible" {
-#  depends_on = [aws_instance.instance,aws_route53_record.dns]
-#  provisioner "remote-exec" {
-#
-#    connection {
-#      type     = "ssh"
-#      user     = "centos"
-#      password = "DevOps321"
-#      host     = aws_instance.instance.public_ip
-#    }
-#
-#    inline = [
-#      "sudo labauto ansible",
-#      "sudo set-hostname -skip-apply ${var.component}",
-#      "ansible-pull -i localhost, -U https://github.com/vjsmit/roboshop-ansible main.yml -e env=${var.env} -e role_name=${var.component}",
-#    ]
-#  }
-#
-#}
 
-resource "aws_security_group" "sg" {
+##security group
+
+resource "aws_security_group" "main" {
   name        = "${var.component}-${var.env}-sg"
   description = "${var.component}-${var.env}-sg"
-  vpc_id      =  var.vpc_id
+
   ingress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port        = var.app_port
+    to_port          = var.app_port
+    protocol         = "tcp"
+    cidr_blocks      = var.sg_subnets_cidr
   }
 
   egress {
@@ -148,12 +24,47 @@ resource "aws_security_group" "sg" {
   }
 }
 
-resource "aws_instance" "test" {
-  ami = data.aws_ami.ami.id
-  instance_type = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.sg.id]
-  subnet_id = var.subnet_id
-  tags = {
-    name = var.component
+resource "aws_launch_template" "main" {
+  name = "${var.component}-${var.env}"
+  iam_instance_profile {
+    name = aws_iam_instance_profile.instance_profile.name
+  }
+  image_id               = data.aws_ami.ami.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.main.id]
+
+  tag_specifications {
+    resource_type = "instance"
+    tags          = merge({ Name = "${var.component}-${var.env}", Monitor = "true" }, var.tags)
+  }
+  user_data = templatefile("${path.module}/userdata.sh", {
+    env       = var.env
+    component = var.component
+  })
+  root_block_device {
+    encrypted = true
+    kms_key_id = var.kms_key_id
   }
 }
+
+resource "aws_autoscaling_group" "main" {
+  desired_capacity   = var.desired_capacity
+  max_size           = var.max_size
+  min_size           = var.min_size
+
+  launch_template {
+    id      = aws_launch_template.main.id
+    version = "$Latest"
+  }
+}
+
+##dns record
+#
+#resource "aws_route53_record" "dns" {
+#  zone_id = "Z0478111B3O54DLF4LSS"
+#  name    = "${var.component}-dev"
+#  type    = "A"
+#  ttl     = 30
+#  records = [aws_instance.instance.private_ip]
+#}
+
--- variables.tf
+++ variables.tf
@@ -5,5 +5,13 @@ variable "component" {}
 variable "tags" {
   default = {}
 }
-variable "subnet_id" {}
+variable "subnets" {}
 variable "vpc_id" {}
+variable "app_port" {}
+variable "sg_subnets_cidr" {}
+variable "kms_key_id" {}
+variable "instance_type" {}
+variable "name" {}
+variable "max_size" {}
+variable "min_size" {}
+variable "desired_capacity" {}
