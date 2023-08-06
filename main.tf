8ee9e1fb69fe76d66911d5b04134fecb7131adf8
vjsmit <vj.smit97@gmail.com>
2023-08-06 23:11:14 +0530

    updating userdata.sh

--- main.tf
+++ main.tf
@@ -38,9 +38,9 @@ resource "aws_lb_target_group" "main" {
   vpc_id   = var.vpc_id
 }
 
-resource "aws_lb_listener_rule" "host_based_weighted_routing" {
+resource "aws_lb_listener_rule" "main" {
   listener_arn = var.listener_arn
-  priority     = var.lb_dns_name
+  priority     = var.lb_rule_priority
 
   action {
     type             = "forward"
