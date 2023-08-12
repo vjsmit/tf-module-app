a636d75e6eaafa57b1c8a23f8c76a2688d3e1daf
vjsmit <vj.smit97@gmail.com>
2023-08-12 08:47:21 +0530

    updating userdata.sh

--- main.tf
+++ main.tf
@@ -35,7 +35,9 @@ resource "aws_lb_target_group" "main" {
   name     = "${var.component}-${var.env}-tg"
   port     = var.app_port
   protocol = "HTTP"
+  deregistration_delay = 30
   vpc_id   = var.vpc_id
+
   health_check {
     enabled = true
     interval = 5
