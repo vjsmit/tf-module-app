b8b267a9bc97b466ba58069b74281be92d5add56
vjsmit <vj.smit97@gmail.com>
2023-09-03 19:20:15 +0530

    updating redis_host endpoint

--- main.tf
+++ main.tf
@@ -61,6 +61,7 @@ resource "aws_autoscaling_policy" "asg-cpu-rule" {
   name                   = "CPULoadDetect"
   autoscaling_group_name = aws_autoscaling_group.main.name
   policy_type            = "TargetTrackingScaling"
+  estimated_instance_warmup = 120
   target_tracking_configuration {
     predefined_metric_specification {
       predefined_metric_type = "ASGAverageCPUUtilization"
